b28ead0dc97c91d0f917a08cc6a4991826794158
pritishsg13@gmail.com <pritishsg13@gmail.com>
2026-04-13 12:03:30 +0530

    promises in js

---
+++ script/script.js
@@ -0,0 +1,58 @@
+// // setTimeout (runs once after delay)
+// setTimeout(function () {
+//     console.log("Hello World");
+// }, 2000);
+
+
+// // setInterval (runs again and again)
+// setInterval(function () {
+//     console.log("Hello Iran and America");
+// }, 1000);
+
+// let promiseData=new Promise((res,rej)=>{
+//     const success=false;
+
+//     setTimeout(()=>{
+//         if(success){
+//             res("success in fetching data")
+//         }
+//         else{
+//             rej("error in fetching");
+//         }
+
+
+//     },2000);
+// });
+
+// promiseData.then((a)=>{
+//     console.log(a);
+//     console.log("then block")
+// });
+
+// promiseData.catch((b)=>{
+//     console.log(b);
+//     console.log("catch block")
+// });
+
+// promiseData.finally(()=>{
+//     console.log("success");
+// })
+
+
+
+//FETCHING
+
+let data=fetch("https://dummyjson.com/products");
+
+data
+.then((a)=>{
+    return a.json(); //a.json() converts response → JavaScript object
+})
+.then((b)=>{
+    let val=b.products; //This .then() runs when a.json() is complete
+// b contains the actual data from API
+    val.forEach((a)=>{
+        console.log(a);
+        console.log(a.description)
+    });
+});
