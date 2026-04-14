adc78761f14645c83483da6c8ddc1f77c79962ec
pritishsg13@gmail.com <pritishsg13@gmail.com>
2026-04-14 14:56:46 +0530

    homogenous array with union code

--- typescript/script.ts
+++ typescript/script.ts
@@ -42,10 +42,49 @@
 
 // function with optional parameter
 
-function demo (a: number, b?:string, c?:boolean){
-    console.log("hello");
-    console.log(a);
-    console.log(b);
-    console.log(c);
+// function demo (a: number, b?:string, c?:boolean){
+//     console.log("hello");
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
     
-}
+// }
+
+
+
+//Homogenous Array
+
+
+
+// Number Array
+// let numbers: number[] = [1, 2, 3];
+// numbers.push(4);
+
+// // String Array
+// let names: string[] = ["Pritish", "Rahul"];
+// names.push("Aman");
+
+// // Boolean Array
+// let flags: boolean[] = [true, false];
+// flags.push(true);
+
+// // Array using Array<Type>
+// let marks: Array<number> = [10, 20, 30];
+// marks.push(40);
+
+// // One console (all outputs together)
+// console.log(numbers, names, flags, marks);
+
+
+
+
+// //Union with Union Parameter
+
+
+// function show(value: string | number | boolean): void {
+//     console.log(value);
+// }
+
+// show("Hi");
+// show(20);
+// show(true);
