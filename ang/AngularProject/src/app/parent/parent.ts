54bfc77b43de648599b53b3ecdd9ffab924b28cd
pritishsg13@gmail.com <pritishsg13@gmail.com>
2026-04-21 11:57:30 +0530

    done with ngclass

--- ang/AngularProject/src/app/parent/parent.ts
+++ ang/AngularProject/src/app/parent/parent.ts
@@ -1,20 +1,69 @@
+// // import { Component } from '@angular/core';
+// // import { ChildComponent } from '../child/child';
+// // import { CommonModule } from '@angular/common';
+
+// // @Component({
+// //   selector: 'app-parent',
+// //   standalone: true,
+// //   imports: [ChildComponent, CommonModule],
+// //   templateUrl: './parent.html'
+// // })
+// // export class ParentComponent {
+
+// //   messageToChild = "Hello Child";
+
+// //   receivedArray: string[] = [];
+
+// //   getDataFromChild(data: string[]) {
+// //     this.receivedArray = data;
+// //   }
+// // }
+// import { Component } from '@angular/core';
+// import { ChildComponent } from '../child/child';
+// import { CommonModule } from '@angular/common';
+
+// @Component({
+//   selector: 'app-parent',
+//   standalone: true,
+//   imports: [ChildComponent, CommonModule],
+//   templateUrl: './parent.html',
+//   styleUrls: ['./parent.css']
+// })
+// export class ParentComponent {
+
+//   messageToChild = "Hello Child";
+
+//   receivedArray: string[] = [];
+
+//   isActive = false;
+
+//   getDataFromChild(data: string[]) {
+//     this.receivedArray = data;
+//   }
+
+//   toggleClass() {
+//     this.isActive = !this.isActive;  // toggle true/false
+//   }
+// }
 import { Component } from '@angular/core';
-import { ChildComponent } from '../child/child';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-parent',
   standalone: true,
-  imports: [ChildComponent, CommonModule],
-  templateUrl: './parent.html'
+  imports: [CommonModule],
+  templateUrl: './parent.html',
+  styleUrls: ['./parent.css']
 })
 export class ParentComponent {
 
-  messageToChild = "Hello Child";
+  act: boolean = true;
+  inact: boolean = false;
 
-  receivedArray: string[] = [];
-
-  getDataFromChild(data: string[]) {
-    this.receivedArray = data;
+  handleClass() {
+    return {
+      active: this.act,
+      inactive: this.inact
+    };
   }
 }
