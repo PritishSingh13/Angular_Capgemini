dfc291f099eae91559f72c5de1108e05107e8720
pritishsg13@gmail.com <pritishsg13@gmail.com>
2026-04-21 12:24:06 +0530

    done with ng style

--- ang/AngularProject/src/app/parent/parent.ts
+++ ang/AngularProject/src/app/parent/parent.ts
@@ -1,50 +1,3 @@
-// // import { Component } from '@angular/core';
-// // import { ChildComponent } from '../child/child';
-// // import { CommonModule } from '@angular/common';
-
-// // @Component({
-// //   selector: 'app-parent',
-// //   standalone: true,
-// //   imports: [ChildComponent, CommonModule],
-// //   templateUrl: './parent.html'
-// // })
-// // export class ParentComponent {
-
-// //   messageToChild = "Hello Child";
-
-// //   receivedArray: string[] = [];
-
-// //   getDataFromChild(data: string[]) {
-// //     this.receivedArray = data;
-// //   }
-// // }
-// import { Component } from '@angular/core';
-// import { ChildComponent } from '../child/child';
-// import { CommonModule } from '@angular/common';
-
-// @Component({
-//   selector: 'app-parent',
-//   standalone: true,
-//   imports: [ChildComponent, CommonModule],
-//   templateUrl: './parent.html',
-//   styleUrls: ['./parent.css']
-// })
-// export class ParentComponent {
-
-//   messageToChild = "Hello Child";
-
-//   receivedArray: string[] = [];
-
-//   isActive = false;
-
-//   getDataFromChild(data: string[]) {
-//     this.receivedArray = data;
-//   }
-
-//   toggleClass() {
-//     this.isActive = !this.isActive;  // toggle true/false
-//   }
-// }
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -66,4 +19,11 @@ export class ParentComponent {
       inactive: this.inact
     };
   }
+
+
+  css = {
+    'background-color': 'black',
+    'color': 'red',
+    'padding': '10px'
+  };
 }
